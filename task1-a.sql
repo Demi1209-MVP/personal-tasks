fea73b80a448e26036baed64ffbd617d74e355c1
Demi Zhao <58715338+Demi1209-MVP@users.noreply.github.com>
2019-12-17 14:03:34 +1300

    Update task1-a.sql

--- task1-a.sql
+++ task1-a.sql
@@ -5,7 +5,7 @@ select * from PropertyOwnerReview
 select * from Address
 
 
---Display a list of all property names and their property id¡¯s for Owner Id: 1426.
+--Display a list of all property names and their property id=Owner Id: 1426.
 
 SELECT o.OwnerId,o.PropertyId,t.Name
 From 
