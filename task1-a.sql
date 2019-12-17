810ecbdce40b397115ee53254dbc90a755846463
Demi Zhao <58715338+Demi1209-MVP@users.noreply.github.com>
2019-12-17 14:03:01 +1300

    Update task1-a.sql

--- task1-a.sql
+++ task1-a.sql
@@ -5,7 +5,7 @@ select * from PropertyOwnerReview
 select * from Address
 
 
---Display a list of all property names and their property id¡¯s for Owner Id: 1426.
+--Display a list of all property names and their property idÂ¡Â¯s for Owner Id: 1426.
 
 SELECT o.OwnerId,o.PropertyId,t.Name
 From 
@@ -13,7 +13,3 @@ OwnerProperty as o
 JOIN Property AS p on o.PropertyId=p.Id
 JOIN PropertyType AS t on p.PropertyTypeId=t.PropertyTypeId
 where o.OwnerId=1426
-
-
-
-Where
